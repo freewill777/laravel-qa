fa22c8651694f62fc5fc9fe34fe96de4a547a74e
Sfetcu <212780951@logon.ds.ge.com>
2019-11-14 11:03:33 +0200

    Form functionality: update items

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
-use App\Http\Requests\AskQuestionRequest;
 
 class QuestionController extends Controller
 {
@@ -40,11 +39,9 @@ class QuestionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(AskQuestionRequest $request)
+    public function store(Request $request)
     {
-        $request->user()->questions()->create($request->only('title','body'));
-
-        return redirect()->route('questions.index')->with('success','Your question has been addded');
+        //
     }
 
     /**
@@ -66,7 +63,7 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+       return view("questions.edit",compact('question'));
     }
 
     /**
@@ -76,9 +73,11 @@ class QuestionController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question>update($request->only('title','body'));
+
+        return redirect('/questions')->with('success','Your item has been updated');
     }
 
     /**
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -31,10 +31,16 @@
                                 </div>
                             </div>
                             <div class="media-body">
+                                <div class="d-flex align-items-center">
                                 <h3 class="mt-0">
                                     <a href="{{ $question -> url  }}">
                                         {{ $question->title}}
                                     </a>
+                                </h3>
+                                <div class="ml-auto">
+                                    <a href="{{route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
+                                </div>
+                                </div>
                                     <p class="lead">
                                         Asked by 
                                         <a href="{{ $question->user->url }}">
@@ -44,7 +50,7 @@
                                             {{$question -> created_date}}
                                         </small>
                                     </p>
-                                </h3>
+                                
                                 {{ $question->body }}
                             </div>
                         </div>
